39e3ceacc4ca8d2a1ea32c16fc64db57b4160b9b
DGIST2023choidoyun <cdydoyun@dgist.ac.kr>
2025-06-03 05:22:33 +0900

    0603 init

---
+++ client.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+// #include <poll.h>
+#include <netdb.h>
+#include <unistd.h>
+// #include <jansson.h>
+
+#include "board.h"
+
+#pragma region TYPEDEF
+// typedef struct pollfd           s_PollFD;
+typedef struct addrinfo         s_Addrinfo;
+typedef char                    bool;
+#pragma endregion
+
+
+
+int main(int argc, char* argv[]) {
+
+
+
+    return 0;
+}
